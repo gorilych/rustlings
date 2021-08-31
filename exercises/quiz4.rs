1685aad576cf2c675cdef36fa2a4c5b410dba1a7
Andrey Kartashov <gorilych@gmail.com>
2021-08-31 23:27:50 +0700

    take 4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,7 +5,11 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($s:expr) => {
+        ("Hello ".to_string() + $s).as_str()
+    }
+}
 
 #[cfg(test)]
 mod tests {
